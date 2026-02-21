9244448063c50f4ea4ecee90c24901f5ccffbc60
AleksandarGekov <soter69420@gmail.com>
2026-02-21 12:01:54 +0200

    refactor: fixed incorrect links

--- c4-plp/src/components/Header/Links.jsx
+++ c4-plp/src/components/Header/Links.jsx
@@ -6,11 +6,10 @@ export default function Links() {
     return (
             <div>
                 <ul className="nav_links">
-                    <li><Link to="/catalog" className="light" >Catalog</Link></li>
-                    <li><Link to="/bags" className="light" >Bags</Link></li>
-                    <li><Link to="/shoes" className="light" >Shoes</Link></li>
-                    <li><Link to="/denim" className="light" >Denim</Link></li>
-                    <li><Link to="/jewellery" className="light" >Jewellery</Link></li>
+                    <li><Link to="/Catalog/Bags" className="light" >Bags</Link></li>
+                    <li><Link to="/Catalog/Shoes" className="light" >Shoes</Link></li>
+                    <li><Link to="/Catalog/Denim" className="light" >Denim</Link></li>
+                    <li><Link to="/Catalog/Jewellery" className="light" >Jewellery</Link></li>
                 </ul >
             </div>
     )
