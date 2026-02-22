9d2766b5bd7f3e17fcb44d25db65370a6c06d3e9
AleksandarGekov <soter69420@gmail.com>
2026-02-22 12:45:57 +0200

    feat: added bg color

--- c4-plp/src/components/Header/Links.jsx
+++ c4-plp/src/components/Header/Links.jsx
@@ -6,10 +6,10 @@ export default function Links() {
     return (
             <div>
                 <ul className="nav_links">
-                    <li><Link to="/Catalog/Bags" className="light" >Bags</Link></li>
-                    <li><Link to="/Catalog/Shoes" className="light" >Shoes</Link></li>
-                    <li><Link to="/Catalog/Denim" className="light" >Denim</Link></li>
-                    <li><Link to="/Catalog/Jewelry" className="light" >Jewelry</Link></li>
+                    <li className="link"><Link to="/Catalog/Bags" className="light" >Bags</Link></li>
+                    <li className="link"><Link to="/Catalog/Shoes" className="light" >Shoes</Link></li>
+                    <li className="link"><Link to="/Catalog/Denim" className="light" >Denim</Link></li>
+                    <li className="link"><Link to="/Catalog/Jewelry" className="light" >Jewelry</Link></li>
                 </ul >
             </div>
     )
